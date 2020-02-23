6f20199361f06108536e8b2c6cef8a0c88651f70
UdayaSri <60940473+udayasri2527@users.noreply.github.com>
2020-02-22 21:37:02 -0800

    Initial commit

---
+++ insta-resize/src/main/java/com/instaresize/instaresize/InstaResizeApplication.java
@@ -0,0 +1,13 @@
+package com.instaresize.instaresize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaResizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaResizeApplication.class, args);
+	}
+
+}
---
+++ insta-resize/src/test/java/com/instaresize/instaresize/InstaResizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instaresize.instaresize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaResizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
